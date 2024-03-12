c5a5470d43cd67d272b35589a92a1c6f1ccbfba9
Fernando Schneider <93660206+fernando-sch@users.noreply.github.com>
2024-03-11 22:27:52 -0300

    Setup exmachina lib (#4)

---
+++ test/support/factory.ex
@@ -0,0 +1,39 @@
+defmodule ShipayInstatus.Factory do
+  @moduledoc """
+    Factory of fixtures using ex-machina lib, for tests
+  """
+  use ExMachina.Ecto, repo: ToDo.Repo
+
+  alias ShipayInstatus.Incident
+
+  def incident_factory do
+    stringified_utc_now = NaiveDateTime.utc_now() |> NaiveDateTime.to_string()
+
+    %Incident{
+      id: sequence("id_"),
+      impact: sequence("impact_"),
+      name: sequence("name_"),
+      status: get_incident_status(),
+      url: sequence("https://some_url_"),
+      resolved_at: stringified_utc_now,
+      updated_at: stringified_utc_now,
+      created_at: stringified_utc_now,
+      incident_updates: build_list(:incident_update)
+    }
+  end
+
+  def incident_update_factory do
+    stringified_utc_now = NaiveDateTime.utc_now() |> NaiveDateTime.to_string()
+
+    %Incident.Update{
+      incident_id: sequence("id_"),
+      body: sequence("body_"),
+      status: get_incident_status(),
+      created_at: stringified_utc_now,
+      updated_at: stringified_utc_now
+    }
+  end
+
+  defp get_incident_status,
+    do: Enum.random("INVESTIGATING", "IDENTIFIED", "MONITORING", "RESOLVED")
+end
