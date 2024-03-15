4f5396259bb06ef1611c7503729b10141a21857d
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-03-15 18:14:53 -0300

    add maintenance and component webhook payload factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -13,7 +13,7 @@ defmodule InstatusAPIConsumer.Factory do
       id: sequence("id_"),
       impact: sequence("impact_"),
       name: sequence("name_"),
-      status: get_incident_status(),
+      status: get_notification_status(),
       url: sequence("https://some_url_"),
       resolved_at: stringified_utc_now,
       updated_at: stringified_utc_now,
@@ -28,13 +28,13 @@ defmodule InstatusAPIConsumer.Factory do
     %Incident.Update{
       incident_id: sequence("id_"),
       body: sequence("body_"),
-      status: get_incident_status(),
+      status: get_notification_status(),
       created_at: stringified_utc_now,
       updated_at: stringified_utc_now
     }
   end
 
-  defp get_incident_status,
+  defp get_notification_status,
     do: Enum.random(["INVESTIGATING", "IDENTIFIED", "MONITORING", "RESOLVED"])
 
   def incident_webhook_payload_factory do
@@ -55,7 +55,7 @@ defmodule InstatusAPIConsumer.Factory do
         "id" => sequence("incident_id_"),
         "name" => sequence("name_"),
         "impact" => sequence("impact_"),
-        "status" => get_incident_status(),
+        "status" => get_notification_status(),
         "url" => sequence("https://incident_url_"),
         "backfilled" => false,
         "created_at" => stringified_utc_now,
@@ -65,10 +65,75 @@ defmodule InstatusAPIConsumer.Factory do
           "id" => sequence("incident_update_id_"),
           "incident_id" => sequence("incident_id_"),
           "body" => sequence("body_"),
-          "status" => get_incident_status(),
+          "status" => get_notification_status(),
           "created_at" => stringified_utc_now,
           "updated_at" => stringified_utc_now,
         }],
+      },
+    }
+  end
+
+  def maintenance_webhook_payload_factory do
+    stringified_utc_now = NaiveDateTime.utc_now() |> NaiveDateTime.to_string()
+
+    %{
+      "meta" => %{
+        "unsubscribe" => sequence("https://unsub_"),
+        "documentation" => "https://docs_",
+      },
+      "page" => %{
+        "id" => sequence("page_id_"),
+        "status_indicator" => sequence("indicator_"),
+        "status_description" => sequence("description_"),
+        "url" => sequence("https://page_url_")
+      },
+      "maintenance" => %{
+        "id" => sequence("incident_id_"),
+        "name" => sequence("name_"),
+        "impact" => sequence("impact_"),
+        "status" => get_notification_status(),
+        "url" => sequence("https://incident_url_"),
+        "duration" => sequence("duration_"),
+        "backfilled" => false,
+        "created_at" => stringified_utc_now,
+        "resolved_at" => stringified_utc_now,
+        "updated_at" => stringified_utc_now,
+        "maintenance_updates" => [%{
+          "id" => sequence("incident_update_id_"),
+          "maintenance_id" => sequence("incident_id_"),
+          "body" => sequence("body_"),
+          "status" => get_notification_status(),
+          "created_at" => stringified_utc_now,
+          "updated_at" => stringified_utc_now,
+        }],
+      },
+    }
+  end
+
+  def component_webhook_payload_factory do
+    stringified_utc_now = NaiveDateTime.utc_now() |> NaiveDateTime.to_string()
+
+    %{
+      "meta" => %{
+        "unsubscribe" => sequence("https://unsub_"),
+        "documentation" => "https://docs_",
+      },
+      "page" => %{
+        "id" => sequence("page_id_"),
+        "status_indicator" => sequence("indicator_"),
+        "status_description" => sequence("description_"),
+        "url" => sequence("https://page_url_")
+      },
+      "component_update" => %{
+        "component_id" => sequence("component_id_"),
+        "new_status" =>  get_notification_status(),
+        "created_at" => stringified_utc_now,
+      },
+      "component" => %{
+        "id" => sequence("component_id_"),
+        "name" => sequence("name_"),
+        "status" => get_notification_status(),
+        "created_at" => stringified_utc_now,
       },
     }
   end
