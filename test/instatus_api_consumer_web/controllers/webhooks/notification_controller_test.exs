8c31da67665aae7488866e1a79b89233f1d4566d
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-03-15 18:15:13 -0300

    add maintenance and component webhook notification matching

--- test/instatus_api_consumer_web/controllers/webhooks/notification_controller_test.exs
+++ test/instatus_api_consumer_web/controllers/webhooks/notification_controller_test.exs
@@ -6,16 +6,23 @@ defmodule InstatusAPIConsumerWeb.Webhooks.NotificationControllerTest do
   alias InstatusAPIConsumer.Incidents.Incident
 
   describe "notification/2" do
-    test "returns 204 no content" do
+    test "returns 204 no content for incident notification" do
       payload = Factory.build(:incident_webhook_payload)
       conn = post(build_conn(), "/api/webhooks/notification", payload)
       assert Repo.exists?(Incident, id: payload["incident"]["id"])
       assert response(conn, 204) == ""
     end
 
-    test "returns 400 bad request" do
-      conn = post(build_conn(), "/api/webhooks/notification", %{})
-      assert json_response(conn, 400) == %{"errors" => %{"detail" => "Bad Request"}}
+    test "returns 204 no content for maintenance notification" do
+      payload = Factory.build(:incident_webhook_payload)
+      conn = post(build_conn(), "/api/webhooks/notification", payload)
+      assert response(conn, 204) == ""
+    end
+
+    test "returns 204 no content for component notification" do
+      payload = Factory.build(:incident_webhook_payload)
+      conn = post(build_conn(), "/api/webhooks/notification", payload)
+      assert response(conn, 204) == ""
     end
   end
 end
