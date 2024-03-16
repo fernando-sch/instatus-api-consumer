a846bdef7dfb456ed6df69ddeeef7b85a9dd42f3
Fernando Schneider <93660206+fernando-sch@users.noreply.github.com>
2024-03-16 00:27:56 -0300

    Remove logger from notification controller (#13)

--- lib/instatus_api_consumer_web/controllers/webhooks/notification_controller.ex
+++ lib/instatus_api_consumer_web/controllers/webhooks/notification_controller.ex
@@ -1,15 +1,13 @@
 defmodule InstatusAPIConsumerWeb.Webhooks.NotificationController do
   use InstatusAPIConsumerWeb, :controller
 
-  require Logger
-
   action_fallback InstatusAPIConsumerWeb.FallbackController
 
   alias InstatusAPIConsumer.Incidents
 
   # Incident notification
   def notification(conn, %{"incident" => incident_params}) do
-    Logger.info("Incident notification: #{incident_params}")
+    IO.inspect("Incident notification: #{incident_params}")
     with {:ok, _} <- Incidents.create_or_update_incident(incident_params) do
       send_resp(conn, :no_content, "")
     end
@@ -17,15 +15,15 @@ defmodule InstatusAPIConsumerWeb.Webhooks.NotificationController do
 
   # Maintenance notification
   def notification(conn, %{"maintenance" => maintenance_params}) do
-    Logger.info("Maintenance notification: #{maintenance_params}")
+    IO.inspect("Maintenance notification: #{maintenance_params}")
 
     send_resp(conn, :no_content, "")
   end
 
   # Component notification
   def notification(conn, %{"component_update" => component_update_params, "component" => component_params}) do
-    Logger.info("Component update notification: #{component_update_params}")
-    Logger.info("Component notification: #{component_params}")
+    IO.inspect("Component update notification: #{component_update_params}")
+    IO.inspect("Component notification: #{component_params}")
 
     send_resp(conn, :no_content, "")
   end
