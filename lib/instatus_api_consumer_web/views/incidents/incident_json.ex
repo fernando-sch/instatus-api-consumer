59056541661ab01405247f68302d6980a773fbfc
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-03-20 16:56:28 -0300

    add incident json view

---
+++ lib/instatus_api_consumer_web/views/incidents/incident_json.ex
@@ -0,0 +1,28 @@
+defmodule InstatusAPIConsumerWeb.Incidents.IncidentJSON do
+  alias InstatusAPIConsumer.Incidents.Incident
+  alias InstatusAPIConsumerWeb.Incidents.IncidentUpdateJSON
+
+  def index(%{incidents: incidents}) do
+    %{data: for(incident <- incidents, do: data(incident))}
+  end
+
+  def show(%{incident: incident}) do
+    %{data: data(incident)}
+  end
+
+  def data(%Incident{incident_updates: incident_updates} = incident) do
+    incident_updates =
+      for(incident_update <- incident_updates, do: IncidentUpdateJSON.data(incident_update))
+
+    %{
+      impact: incident.impact,
+      name: incident.name,
+      status: incident.status,
+      url: incident.url,
+      resolved_at: incident.resolved_at,
+      updated_at: incident.updated_at,
+      created_at: incident.created_at,
+      incident_updates: incident_updates
+    }
+  end
+end
