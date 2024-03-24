27c2d345d32f605f4d725a0b965b21c97bb88a2a
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-03-24 16:38:44 -0300

    add oban library

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule InstatusAPIConsumer.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:plug_cowboy, "~> 2.5"},
-      {:cors_plug, "~> 3.0"}
+      {:cors_plug, "~> 3.0"},
+      {:oban, "~> 2.17"}
     ]
   end
 
