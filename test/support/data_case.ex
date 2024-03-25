cf68ad72c0eebf04cabeeaba69a656414ee9f079
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-03-25 16:16:25 -0300

    fix formatter

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -37,8 +37,7 @@ defmodule InstatusConsumer.DataCase do
   Sets up the sandbox based on the test tags.
   """
   def setup_sandbox(tags) do
-    pid =
-      Ecto.Adapters.SQL.Sandbox.start_owner!(InstatusConsumer.Repo, shared: not tags[:async])
+    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(InstatusConsumer.Repo, shared: not tags[:async])
 
     on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
   end
